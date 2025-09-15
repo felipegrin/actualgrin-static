1dc8e890692b9b6353f1b4b7d52158b1bea91be0
Ephraim Grinsztajn <ephraimgrin@gmail.com>
2025-09-15 13:10:05 +0300

    commit

--- netlify/functions/getBudget.js
+++ netlify/functions/getBudget.js
@@ -3,12 +3,12 @@ const api = require('@actual-app/api');
 exports.handler = async () => {
   try {
     await api.init({
-      serverURL: 'http://your-actual-budget-server:5006',
+      serverURL: "https://actualgrin.pikapod.net",
       password: process.env.ACTUAL_PASSWORD, // store password in env variables
       dataDir: '/tmp', // temporary storage for serverless functions
     });
 
-    const budget = await api.downloadBudget('your_budget_id');
+    const budget = await api.downloadBudget('1bc93ff2-c30a-4f25-9c36-8572ba72df56');
 
     return {
       statusCode: 200,
